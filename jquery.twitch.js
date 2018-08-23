439c932324bede004138c1443c76c6e2523f8f5e
Mark <klynicol@users.noreply.github.com>
2018-08-22 19:01:38 -0700

    Add files via upload

--- jquery.twitch.js
+++ jquery.twitch.js
@@ -9,9 +9,9 @@
      $.fn.twitch = function(options) {
 
           let settings = $.extend({
-               layers: 0,
+               layers: 1,
                distance: 20,
-               transition: 1
+               transition: 0.3
           }, options);
 
           //Errors and fail safes
@@ -21,17 +21,6 @@
           else if(settings.distance < 0) settings.distance = 0;
           if(settings.transition < 0) settings.distance = 0;
 
-          
-          //Creating a ticker for animations to run through before the next position is given
-          // let start = Date.now();
-          // let tick = 0;
-          // let lastTick = 0;
-          // setInterval(function(){
-          //      let delta = Date.now() - start;
-          //      tick = Math.floor(delta/(settings.transition * 1000));
-          //      console.log("this thing is ticking");
-          // }, (settings.transition * 1000));
-
           return this.each(function(){
 
                //data for each instance passed
@@ -39,9 +28,6 @@
                let children = []; //store children so they can be killed later
                each.data("position", each.position());
                each.data("able", true);
-               //each.data("children", []);
-               //each.data("isTwitched", false);
-               //let relative = each.position();
                
                each.mousemove(function(e) { //only applies to parent elements
                     let setTwitch = function(element, layersLeft, returnPostion){
@@ -50,62 +36,61 @@
                          let pageX = e.pageX - offset.left;
                          let pageY = e.pageY - offset.top;
 
+                         //test variable
+                         let id = element.attr('id');
+
                          if (element.data("able")) { //if element is ready to be twitched again according to the transitions set
                               //set timer for able to execute again
                               setTimeout(function(){
                                    element.data("able", true);
-                                   console.log("test1")
                               }, (settings.transition * 1000));
-
-                              
-                              
-
-                              //temporarily hide element to check for element beneath and do a twitch on them
-                              element.css('display', 'none');
-                              let under = $(document.elementFromPoint(e.clientX, e.clientY)); //under = new element
-                              if(under.is('.hover-twitch') && layersLeft > 0){
-                                   console.log("check1");
-                                   under.data("able", true);
-                                   under.data("position", under.position());
-                                   setTwitch(under, (layersLeft - 1), under.position());
-                              }
-                              element.css('display', 'inline-block');//unhide element again
 
                               //setting transition speed
                               element.css('transition', 'all ' + settings.transition + 's');
 
+                              //temporarily hide element to check for element beneath and do a twitch on them
+                              element.css('display', 'none');
+                              let under = $(document.elementFromPoint(e.clientX, e.clientY)); //under = new element
+                              if(under.is('.' + element.attr('class')) && layersLeft > 0){
+                                   children.push(under);
+                                   if(!under.data("able")) under.data("able", true);
+                                   if(!under.data("position")) under.data("position", under.position());
+                                   setTwitch(under, (layersLeft - 1), under.data("position"));
+                              }
+                              element.css('display', 'inline-block');//unhide element again
+
                               //twitch the element
                               let executeTwitch = function(quadrant, direction){
                                    let x = direction == "positive" ? (parseInt(returnPostion[quadrant]) + settings.distance) : (parseInt(returnPostion[quadrant]) - settings.distance);
-                                   element.css('transition', 'all 1s');
                                    element.css(quadrant, x + 'px');
                               }
                               if (pageX < (element.width()/2)) executeTwitch("left", "positive"); else executeTwitch("left", "negative"); //twitch x
                               if (pageY > (element.height()/2)) executeTwitch("top", "negative"); else executeTwitch("top", "positive"); //twitch y
-
-                              //mouse leave conditional kill parent and all children
-                              element.mouseleave(function(){
-                                   //console.log("mouse leave event fired");
-                                   element.css({
-                                        top : returnPostion.top,
-                                        left : returnPostion.left
-                                   });
-                                   if(children){
-                                        for (var i = 0; i < children.length; i++) {
-                                             let child = children[i];
-                                             child.css({
-                                                  top : child.data("position").top,
-                                                  left : child.data("position").left
-                                             });
-                                        }
-                                   }
-                              });
+                              //disable moving the element until the timer has gone off
                               element.data("able", false);
                          }
-                    }
+                    }//END OF SET TWITCH FUNCTION
                     //do the twitch on the original element
                     setTwitch(each, settings.layers, each.data("position"));
+               }); //END OF MOUSEMOVE EVENT
+               //mouse leave conditional kill parent and all children
+               each.mouseleave(function(){
+                    console.log("mouse leave event fired");
+                    each.css({
+                         top : each.data("position").top,
+                         left : each.data("position").left
+                    });
+                    if(children){
+                         for (var i = 0; i < children.length; i++) {
+                              let child = children[i];
+                              child.css({
+                                   top : child.data("position").top,
+                                   left : child.data("position").left
+                              });
+                         }
+                    }
+                    children = [];
                });
-          });
+          });//END OF EACH!!
      }
 }(jQuery));
