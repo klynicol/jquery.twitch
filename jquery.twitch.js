826f62f5238cd631f76273d8535a53bffdf101fd
Mark <klynicol@users.noreply.github.com>
2018-10-02 14:01:25 -0500

    Add files via upload

--- jquery.twitch.js
+++ jquery.twitch.js
@@ -4,7 +4,6 @@
  *
  * this source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- * Compiled from es6
  **/
 
 /**
@@ -21,48 +20,59 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 (function ($) {
     var Twitch = function () {
-        function Twitch(query, z, speed, distance) {
+        function Twitch(query, z, speed, distance, positionType) {
             _classCallCheck(this, Twitch);
 
             this.query = query; //jquery element
             this.able = true;
             this.child = null;
             this.position = query.position();
-            this.positionMax = null;
+            //this.positionMax = null;
             this.z = z;
             this.speed = speed;
             this.distance = distance;
+            function extractCSS(string) {
+                if (string.includes("px")) {
+                    return Number(string.replace("px", ""));
+                } else {
+                    console.log("%cWhen using relative positioning css values need to be in px", "color: red");
+                    return null;
+                }
+            }
+            this.top = positionType === "relative" ? extractCSS(this.query.css("top")) || 0 : this.position.top;
+            this.left = positionType === "relative" ? extractCSS(this.query.css("left")) || 0 : this.position.left;
             this.ID = function () {
                 return '_' + Math.random().toString(36).substr(2, 9);
             }();
         }
 
         _createClass(Twitch, [{
-            key: 'engage',
+            key: "engage",
             value: function engage(position) {
                 var _this = this;
 
                 this.able = false;
                 this.query.css({
-                    top: position.top,
-                    left: position.left
+                    top: this.top + position.top,
+                    left: this.left + position.left
                 });
                 setTimeout(function () {
                     _this.able = true;
-                    _this.positionMax = _this.query.position();
+                    //this.positionMax = this.query.position();
                 }, this.speed * 1000);
                 return position;
             }
         }, {
-            key: 'disengage',
+            key: "disengage",
             value: function disengage() {
+                //console.log("Disengage on ", this.ID);
                 this.query.css({
-                    top: this.position.top,
-                    left: this.position.left
+                    top: this.top,
+                    left: this.left
                 });
             }
         }, {
-            key: 'getID',
+            key: "getID",
             get: function get() {
                 return this.ID;
             }
@@ -72,17 +82,18 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     }();
 
     $.fn.twitch = function (options) {
-        if ((typeof options === 'undefined' ? 'undefined' : _typeof(options)) === "object") {
+        if ((typeof options === "undefined" ? "undefined" : _typeof(options)) === "object") {
             //settings
             var settings = $.extend({
                 layers: 1,
                 distance: 20,
-                transition: 0.3
+                transition: 0.2,
+                positionType: "absolute"
             }, options);
 
             //Errors and fail safes
-            if (settings.layers < 0) throw new Error("Twittch layers cannot be lower than 0");else if (settings.layers > 4) throw new Error("Twitch layers cannot be larger than 4");else if (settings.layers > 1000) throw new Error("Twitch distance cannot be larger than 1000px");
-            if (settings.distance < 0) settings.distance = 0;
+            if (settings.layers < 0) throw new Error("Twittch layers cannot be lower than 0");else if (settings.layers > 4) throw new Error("Twitch layers cannot be larger than 4");
+            if (settings.distance > 1000) throw new Error("Twitch distance cannot be larger than 1000px");else if (settings.distance < 0) settings.distance = 0;
             if (settings.transition < 0) settings.transition = 0;
 
             var twitchObjects = [];
@@ -91,31 +102,32 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
             return this.each(function () {
                 //init each
-                var current = new Twitch($(this), index, settings.transition, settings.distance); //twitch object
+                var current = new Twitch($(this), index, settings.transition, settings.distance, settings.positionType); //twitch object
                 twitchObjects.push(current);
-                console.log(twitchObjects);
                 index++;
                 current.query.data("twitchID", current.getID); //is a twitch element??
                 current.query.css('transition', 'all ' + settings.transition + 's');
 
                 var executeTwitch = function executeTwitch(element, quadrant, direction) {
-                    return direction == "positive" ? parseInt(element.position[quadrant]) + element.distance : parseInt(element.position[quadrant]) - element.distance;
+                    return direction == "positive" ? +element.distance : -element.distance;
                 };
 
                 var setTwitch = function setTwitch(element, layersLeft, e) {
                     if (layersLeft > 0 && element) {
                         element.query.hide(); //temp hide current
                         var underID = $(document.elementFromPoint(e.clientX, e.clientY)).data("twitchID") || null;
-                        console.log(underID);
-                        if (underID && layersLeft > 0) {
+                        if (underID) {
                             if (!element.child || element.child.ID !== underID) {
                                 //set new child
+                                if (element.child) element.child.disengage();
                                 element.child = function () {
                                     for (var x in twitchObjects) {
                                         if (twitchObjects[x].ID === underID) return twitchObjects[x];
                                     }
                                 }();
-                                console.log(element.child);
+                                //console.log(element.child);
+                            } else if (layersLeft === 1) {
+                                element.child.disengage();
                             }
                             setTwitch(element.child, layersLeft - 1, e);
                         } else {
@@ -135,23 +147,24 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
                         var yDirection = pageY < element.query.height() / 2 ? "positive" : "negative";
                         element.engage({ left: executeTwitch(element, "left", xDirection), top: executeTwitch(element, "top", yDirection) });
                     } else {
-                        console.log("Out of layers, or no twitchObject found");
+                        console.log("%cOut of layers, or no twitchObject found", "color: red");
                     }
                 };
 
                 //////////////////////////////////////////
                 current.query.mousemove(function (e) {
                     if (transfer && transfer.z > current.z) {
-                        //element is below current
+                        //last is below current
                         current.child = transfer;
-                    } else if (transfer && transfer.z < current.z) {//element is above current
-                        //TODO
+                    } else if (transfer && transfer.z < current.z) {
+                        //last is above current
+                        transfer.disengage();
                     } else {
-                            //TODO
-                        }
+                        //TODO
+                    }
                     if (current.able) {
-                        //console.log("Base element is able");
                         setTwitch(current, settings.layers, e);
+                        //console.log(current.child.ID);
                     }
                 }); //END OF MOUSEMOVE EVENT
                 /////////////////////////////////////////////
@@ -161,12 +174,13 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
                         transfer = current;
                     } else {
                         current.disengage();
+                        if (current.child) current.child.disengage();
                         transfer = null;
                     }
                 }); //END OF MOUSELEAVE
             }); //END OF EACH!!
         } else {
-            throw new Error("options needs to be an objet");
+            throw new Error("Twitch options needs to be an objet");
         }
     };
 })(jQuery);
